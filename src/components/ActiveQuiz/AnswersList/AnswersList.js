e1f53f38bf1813a291113eac14a1d9cc2d7aa856
Daniel <daniil.pron4enkov@gmail.com>
2020-04-17 16:55:02 +0300

    react-quiz

---
+++ src/components/ActiveQuiz/AnswersList/AnswersList.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import classes from './AnswersList.css';
+
+import AnswersItem from './AnswerItem/AnswerItem';
+
+const AnswersList = props => {
+    return (
+        <ul className = { classes.AnswersList }>
+            { props.answers.map((answer, index) => {
+                return (
+                    <AnswersItem  
+                    key={ index } 
+                    answer = { answer } 
+                    onAnswerClick = {props.onAnswerClick}
+                    state= {props.state ? props.state[answer.id] : null}
+                    />
+                );
+            }) }
+        </ul>
+    );
+}
+
+export default AnswersList;
